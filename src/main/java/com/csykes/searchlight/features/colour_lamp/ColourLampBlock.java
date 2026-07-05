7a96454aeadde35c623c71ac3700532da8d5def7
Cait <caitlinridgesykes@outlook.com>
2026-07-05 16:52:58 +0100

    + in case i need to revert

--- src/main/java/com/csykes/searchlight/features/colour_lamp/ColourLampBlock.java
+++ src/main/java/com/csykes/searchlight/features/colour_lamp/ColourLampBlock.java
@@ -6,7 +6,6 @@ import com.csykes.searchlight.utils.lighting.BrightnessStage;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import lombok.Getter;
-import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.DyeColor;
@@ -16,9 +15,11 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -27,6 +28,7 @@ import org.jetbrains.annotations.Nullable;
 public class ColourLampBlock extends AbstractLightBlock implements EntityBlock {
     private final DyeColor blockColor;
     public static final IntegerProperty CONNECT_POINTS = IntegerProperty.create("connect_points", 0, 63);
+    public static final BooleanProperty WIDE_CLUSTER = BooleanProperty.create("wide_cluster");
 
     @Override
     public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
@@ -39,13 +41,14 @@ public class ColourLampBlock extends AbstractLightBlock implements EntityBlock {
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(LIT, true)
                 .setValue(BRIGHTNESS, BrightnessStage.MEDIUM)
-                .setValue(CONNECT_POINTS, 0));
+                .setValue(CONNECT_POINTS, 0)
+                .setValue(WIDE_CLUSTER, false)); // Default to false, needed as a way to differentiate between wide and narrow clusters
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(CONNECT_POINTS);
+        builder.add(CONNECT_POINTS, WIDE_CLUSTER);
     }
 
     private int calculateConnectionMask(LevelAccessor level, BlockPos pos, BlockState state) {
@@ -83,11 +86,44 @@ public class ColourLampBlock extends AbstractLightBlock implements EntityBlock {
         return baseState.setValue(CONNECT_POINTS, mask);
     }
 
+    private boolean checkStructureContext(LevelAccessor level, BlockPos pos, int mask) {
+        System.out.println("MASKKKKK " + mask);
+        // Top-Left Corner (Mask = 1 | 2, i.e., 36)
+        if (mask == 36) {
+            System.out.println("Top-Left Corner");
+            System.out.println(level.getBlockState(pos.south().east()).getBlock());
+            return level.getBlockState(pos.south().east()).getBlock() instanceof ColourLampBlock;
+        }
+
+        // Top-Right Corner (Mask = 1 | 8, i.e., 9)
+        if (mask == 33) {
+            return level.getBlockState(pos.south().west()).getBlock() instanceof ColourLampBlock;
+        }
+
+        // Bottom-Left Corner (Mask = 4 | 2, i.e., 6)
+        if (mask == 20) {
+            return level.getBlockState(pos.above().east()).getBlock() instanceof ColourLampBlock;
+        }
+
+        // Bottom-Right Corner (Mask = 4 | 8, i.e., 12)
+        if (mask == 17) {
+            return level.getBlockState(pos.above().west()).getBlock() instanceof ColourLampBlock;
+        }
+        return false;
+    }
+
 
     @Override
     public @NotNull BlockState updateShape(BlockState state, @NotNull Direction direction, @NotNull BlockState neighborState,
                                            @NotNull LevelAccessor level, @NotNull BlockPos pos, @NotNull BlockPos neighborPos) {
-        return state.setValue(CONNECT_POINTS, calculateConnectionMask(level, pos, state));
+
+        int mask = calculateConnectionMask(level, pos, state);
+        if (state.getValue(CONNECT_POINTS) == mask) return state;
+
+        boolean wideCluster = checkStructureContext(level, pos, mask);
+
+        return state.setValue(CONNECT_POINTS, mask)
+                .setValue(WIDE_CLUSTER, wideCluster);
     }
 
     @Override
