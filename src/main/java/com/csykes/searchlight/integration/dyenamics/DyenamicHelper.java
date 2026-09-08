4324f68f8353e590f2919fff5342af3c18cec065
AdamM <adam@mathieson.dev>
2026-09-09 00:29:25 +0200

    + cleanup the locales  + clean up slabs  + fix dyenamics held items

--- src/main/java/com/csykes/searchlight/integration/dyenamics/DyenamicHelper.java
+++ src/main/java/com/csykes/searchlight/integration/dyenamics/DyenamicHelper.java
@@ -6,7 +6,7 @@ public class DyenamicHelper {
     public static int getDyenamicColor(String name) {
         for (DyenamicDyeColor color : DyenamicDyeColor.dyenamicValues()) {
             if (color.getSerializedName().equals(name)) {
-                return color.getFireworkColor();
+                return 0xFF000000 | color.getFireworkColor();
             }
         }
         return -1;
